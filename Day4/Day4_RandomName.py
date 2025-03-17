2c77b5450bffb205bb78075e8ef964e919960816
vAlex2001 <vacarualexandruioan@gmail.com>
2025-03-17 18:04:18 +0200

    2nd Session of learning

---
+++ Day4/Day4_RandomName.py
@@ -0,0 +1,5 @@
+import random
+
+friends = ["A", "B", "C", "D", "E"]
+
+print(random.choice(friends))
---
+++ Day4/Rock_Papper_Scissors.py
@@ -0,0 +1,52 @@
+import random
+
+# Rock
+Rock = ("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+
+# Paper
+Paper = ("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+# Scissors
+Scissors = ("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+
+options = [Rock, Paper, Scissors]
+
+human_choice = int(input("Choose your option 0 - > Rock, 1 -> Paper, 2 -> Scissors : "))
+
+if human_choice > 2 and human_choice < 0:
+    print("Wrong value.")
+else:
+    computer_choice = random.randint(0,2)
+    print("Human Choice: \n" + options[human_choice] + "\n")
+    print("Computer Choice: \n" + options[computer_choice] + "\n")
+    if human_choice == computer_choice:
+        print("Draw")
+    elif human_choice == 0 and computer_choice == 2:
+        print("Win")
+    elif human_choice == 2 and computer_choice == 0:
+        print("Loose")
+    elif human_choice > computer_choice:
+        print("Win")
+    elif human_choice < computer_choice:
+        print("Loose")
