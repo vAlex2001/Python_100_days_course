a814d7fab7240a566d3bef37cb1e4f70d33e081d
vAlex2001 <vacarualexandruioan@gmail.com>
2025-03-26 18:00:40 +0200

    2nd learning session 26.03.2025

---
+++ Day21_Snake_Game_Part2/main.py
@@ -0,0 +1,56 @@
+import turtle
+import Snake
+import time
+import food
+import Scoreboard
+
+# Create the screen
+screen = turtle.Screen()
+screen.tracer(0)
+# Custom size
+screen.setup(width = 600, height = 600)
+
+# Black background
+screen.bgcolor("black")
+
+# Set the title
+screen.title("Snake")
+
+screen.listen()
+
+snake = Snake.Snake()
+food = food.Food()
+scoreboard = Scoreboard.Scoreboard()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+        
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+        
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+# Exit game when click the window
+screen.exitonclick()
