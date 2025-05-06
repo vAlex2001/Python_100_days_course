aaeccfd8d1fe4b15c1c98c762b7b8b70ed6aef25
vAlex2001 <vacarualexandruioan@gmail.com>
2025-05-06 17:47:12 +0300

    price tracker

---
+++ Day47_Amazon_Price_Tracker/main.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+# Add the os and dotenv modules
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+practice_url = "https://appbrewery.github.io/instant_pot/"
+live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+# ====================== Add Headers to the Request ===========================
+
+# Full headers would look something like this
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
+    "Dnt": "1",
+    "Priority": "u=1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
+
+response = requests.get(practice_url, verify=False)
+
+soup = BeautifulSoup(response.content, "html.parser")
+# print(soup.prettify())
+
+# Find the HTML element that contains the price
+price = soup.find(class_="a-offscreen").get_text()
+
+# Remove the dollar sign using split
+price_without_currency = price.split("$")[1]
+
+# Convert to floating point number
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+# ====================== Send an Email ===========================
+
+# Get the product title
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+# Set the price below which you would like to get a notification
+BUY_PRICE = 100
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+
+    # ====================== Use environment variables ===========================
+    
+    with smtplib.SMTP(os.environ["SMTP_ADDRESS"], port=587) as connection:
+        connection.starttls()
+        result = connection.login(os.environ["EMAIL_ADDRESS"], os.environ["EMAIL_PASSWORD"])
+        connection.sendmail(
+            from_addr=os.environ["EMAIL_ADDRESS"],
+            to_addrs=os.environ["EMAIL_ADDRESS"],
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
